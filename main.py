cb141c9b13ac5cfbf24a3ec869131b99f1f281eb
kimminseok <bjm7701@naver.com>
2023-10-22 22:56:23 +0900

    정리 완료

--- main.py
+++ main.py
@@ -14,9 +14,14 @@ if __name__ == "__main__":
     fileNames = [f for f in os.listdir(folderPath) if os.path.isfile(os.path.join(folderPath, f))]
     for fileName in fileNames:
         try:
+            #기업이름 찾기
             logger.info(f"{fileName:20}/{parseCompanyNameFromXml(fileName):10}")
-            logger.info(parseAmountFromXml(fileName))
-            logger.info(parseRatioFromXml(fileName))
+            #청약 주식단위 찾기
+            amountList = parseAmountFromXml(fileName)
+            logger.info(amountList)
+            #비율 찾기
+            ratio = parseRatioFromXml(fileName)
+            logger.info(ratio)
         except UnicodeDecodeError as e:
             logger.error(fileName +"  "+ "UnicodeDecodeError")
         except IndexError as e:
